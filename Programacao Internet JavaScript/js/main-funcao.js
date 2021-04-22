1df6742da7b4dced5fda454c1b2dbea6915a22cf
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-04-22 00:13:16 -0300

    manipulando elementos da pagina

--- Programacao Internet JavaScript/js/main-funcao.js
+++ Programacao Internet JavaScript/js/main-funcao.js
@@ -1,7 +1,40 @@
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado!</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar")
+}
+
+function redirecionar() {
+    window.open("https://www.google.com/");
+    window.location.href = "https://www.google.com/";
+}
+
+function trocar(elemento) {
+   // document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+   elemento.innerHTML =  "Obrigado por passar o mouse";
+   //alert("trocar texto");
+}
+
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML =  "Passe o mouse aqui";
+}
+
+function load() {
+    alert("Pagina carregada");
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value);
+}
+
+/*
 function soma(n1, n2) {
     return n1 + n2;
 }
+*/
 
+/*
 function setReplace(frase, nome, novo_nome) {
     return frase.replace(nome, novo_nome);
 }
@@ -23,3 +56,4 @@ console.log(validaIdade(idade));
 alert(soma(5, 10));
 alert(soma("Vai japão", "Japão", "Brasil"));
 
+*/
